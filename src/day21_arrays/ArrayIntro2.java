047313e4e58df28ba9a3a65899c2eef8d38f7e32
skvozi <nazarii.duvanov@gmail.com>
2024-12-05 19:22:36 -0800

    Push to the develop

---
+++ src/day21_arrays/ArrayIntro2.java
@@ -0,0 +1,9 @@
+package day21_arrays;
+
+public class ArrayIntro2 {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/day21_arrays/fhmh.java
@@ -0,0 +1,11 @@
+package day21_arrays;
+
+public class fhmh {
+    public static void main(String[] args) {
+
+        String s1 = "wooden spoon";
+        String s2 = "" + s1.substring(s1.indexOf("s"));
+        System.out.println(s2);
+
+    }
+}
