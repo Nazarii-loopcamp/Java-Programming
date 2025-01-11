c557bcbf346a31b8264ef7ff9602299ee9f087e3
skvozi <nazarii.duvanov@gmail.com>
2025-01-11 12:26:20 -0800

    poly, exceptions

---
+++ src/day35_inheritance/super_paranthesis/C.java
@@ -0,0 +1,12 @@
+package day35_inheritance.super_paranthesis;
+
+public class C extends A {
+
+    //public C() {
+    //  super();
+    //}
+    public C(double d) {
+        super(d);
+    }
+
+}
---
+++ src/day38_a_abstraction_inheritance/animal/Bird.java
@@ -0,0 +1,10 @@
+package day38_a_abstraction_inheritance.animal;
+
+// Concrete class / Non-Abstract class
+public abstract class Bird extends  Animal{
+
+    int beakSize;
+
+    public abstract void useWings();
+
+}
